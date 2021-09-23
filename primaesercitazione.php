13e86aca219a8c6ad6c9eee38a5470b1c0a548a7
Nicola Cristian <nicola.cristian@osdbsesto>
2021-09-23 13:27:14 +0200

    init repo

---
+++ primaesercitazione.php
@@ -0,0 +1,30 @@
+
+<?php> 
+$a=10;
+$b=159;
+
+$c="hola";
+
+echo 'somma di $a + $b <br>';
+print $a+$b.'<br>';
+echo  $a+$b.'<br>';
+
+
+echo 'somma di $a + $b + $c<br>';
+print $a+$b+$c.'<br>';
+echo  $a+$b+$c.'<br>';
+
+
+echo 'somma di $a + $b concatenato a $c $c<br>';
+print $a+$b.$c.'<br>';
+echo  $a+$b.$c.'<br>';
+
+
+echo 'posso passare più argomenti <br>';
+echo $a+$b,$c.'<br>';
+
+
+echo 'valore rilasciato dal print'
+echo print $a+$b.$c,'<br>',
+'è il risultato del print';
+?> 
